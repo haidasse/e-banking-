5965258d59a1aebf1fd9668d4641df442728282a
haidasse <j.haidasse@upm.ac.ma>
2023-12-15 16:29:47 +0100

    myScendCommit

--- src/main/java/org/sid/ebankingbackend/services/BankAccountServiceImpl.java
+++ src/main/java/org/sid/ebankingbackend/services/BankAccountServiceImpl.java
@@ -2,36 +2,61 @@ package org.sid.ebankingbackend.services;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.sid.ebankingbackend.entities.BankAccount;
+import org.sid.ebankingbackend.entities.CurrentAccount;
 import org.sid.ebankingbackend.entities.Customer;
+import org.sid.ebankingbackend.entities.SavingAccount;
 import org.sid.ebankingbackend.repositories.AccountOperationRepository;
 import org.sid.ebankingbackend.repositories.BankAccountRepository;
 import org.sid.ebankingbackend.repositories.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
 @AllArgsConstructor
+@Slf4j
 public class BankAccountServiceImpl implements BankAccountService {
 
     private CustomerRepository customerRepository;
     private  BankAccountRepository bankAccountRepository;
     private AccountOperationRepository accountOperationRepository;
-    Logger log=LoggerFactory.getLogger(this.getClass().getName());
+
     @Override
-    public Customer saveCustomer(Customer Customer) {
+    public Customer saveCustomer(Customer customer) {
+        log.info("saving new customer");
+        customerRepository.save(customer);
         return null;
     }
 
     @Override
     public BankAccount saveBankAccount(double initialBalanc, String type, Long customerId) {
+        BankAccount bankAccount;
+          Customer customer = customerRepository.findById(customerId).orElse(null);
+        if (type.equals("current")){
+            bankAccount =new CurrentAccount();
+        }else {
+            bankAccount=new SavingAccount();
+        }
+
+        bankAccount.setId(UUID.randomUUID().toString());
+        bankAccount.setCreatedAt(new Date());
+        bankAccount.setBalance(initialBalanc);
+        bankAccount.setCustomer(customer);
+
         return null;
     }
+
+
 
     @Override
     public List<Customer> listCustomer() {
